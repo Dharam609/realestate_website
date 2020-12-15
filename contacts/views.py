0d0614cce63310e4661b3c0d2891ab2455dbbb2f
dharam609 <kcdharam.b@gmail.com>
2020-12-15 16:59:25 -0500

    The complete project commit.

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -11,13 +13,31 @@ def contact(request):
         phone = request.POST['phone']
         message = request.POST['message']
         user_id = request.POST['user_id']
-        # realtor_email = request.POST['realtor_eamil']
-        owner_email = request.POST['owner_email']
+        realtor_email = request.POST['realtor_email']
 
-        contact = Contact(listing=listing, listing_id=listing_id, name=name, owner_email=owner_email,
-        phone=phone, message=message, user_id=user_id)
+        # Check for existing submission
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    request, 'You have already submitted an inquiry for this listing')
+                return redirect('/listings/' + listing_id)
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                          email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
 
-        messages.success(request, 'Your request has been submitted, an owner/a realtor will get back to you ASAP.')
-        return redirect('/listings/'+ listing_id)
+        # Send email
+        send_mail(
+            'Property Inquiry',
+            f'You have a new inquiry for {listing}. Sign into the admin dashboard for more info',
+            'dharamincanada@gmail.com',
+            [realtor_email, 'dharamincanada@gmail.com'],
+            fail_silently=False
+        )
+        messages.success(
+            request, 'Your request has been submitted, a realtor will get to you in 2-3 business days')
+        return redirect('/listings/'+listing_id)
